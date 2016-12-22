847adf437fe0a08a6653e21c301178e0b773d0c8
qweasdzxcpoi <chandan.1si13is016@gmail.com>
2016-12-22 23:34:19 +0530

    This program is used to detect an object of a particular color

---
+++ ObectDetection/src/objectDetecion/ObjRecognition.java
@@ -0,0 +1,52 @@
+package objectDetecion;
+
+import org.opencv.core.*;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+public class ObjRecognition extends Application
+{
+	/**
+	 * The main class for a JavaFX application. It creates and handles the main
+	 * window with its resources (style, graphics, etc.).
+	 * 
+	 * This application looks for any tennis ball in the camera video stream and
+	 * try to select them according to their HSV values. Found tennis balls are
+	 * framed with a blue line.
+	 */
+	@Override
+	public void start(Stage primaryStage)
+	{
+		try
+		{
+			// load the FXML resource
+			
+			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getClassLoader().getResource("ObjRecognition.fxml"));
+			// set a whitesmoke background
+			root.setStyle("-fx-background-color: whitesmoke;");
+			// create and style a scene
+			Scene scene = new Scene(root, 800, 600);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			// create the stage with the given title and the previously created
+			// scene
+			primaryStage.setTitle("Object Recognition");
+			primaryStage.setScene(scene);
+			// show the GUI
+			primaryStage.show();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		// load the native OpenCV library
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		launch(args);
+	}
+}
